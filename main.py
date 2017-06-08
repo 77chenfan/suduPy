f1c0e0f521780d81114c30d0c1e5ee12ce96bc86
fan.chen <fan.chen@tr.com>
2017-06-08 20:13:54 +0800

    add more functions for square

--- main.py
+++ main.py
@@ -10,5 +10,7 @@ import baseSquare as bs
 
 if __name__ == "__main__":
     one =bs.baseSquare()
+    a=[1,2,3,4,5,6,7,8]
+    one.removePossibleValue(a)
+    print one.getPossibleValue()
     print one.getValue()
-    
--- square.py
+++ square.py
@@ -10,7 +10,7 @@ class square(object):
         self.value=0
         self.pox=0
         self.poy=0
-        self.possibleValue=[1,2,3,4,5,6,7,8,9]
+        self.possibleValue={1:1,2:1,3:1,4:1,5:1,6:1,7:1,8:1,9:1}
     def setValue(self,v):
         self.value=v
     def getValue(self):
@@ -25,16 +25,29 @@ class square(object):
         self.setPoy(y)    
     def getPos(self):
         return (self.pox,self.poy)  
-    def addPossibleValue(self,p):
-        self.possibleValue.append(p)
-    
-    def removePossibleValue(self,p):
-        self.possibleValue.remove(p)
-        assert self.possibleValue.count >0, "possible value is empty, failed"
-        if(self.possibleValue.count ==1):
-            self.value = self.possibleValue.pop
+    def updatePossibleValue(self,ps):
+        for k in ps.keys:
+            assert (k<10 and k>0),"please set number between 1 to 9"
+            assert (ps[k]==0 or ps[k]==1), "value only support 1 or 0"
+        self.possibleValue.update(ps)
+        self.__updatePossibleValue()
+    def removePossibleValue(self,ps):
+        if(type(ps)==list):
+            for i in ps:
+                self.possibleValue.pop(i)
+        elif(type(ps)== int):
+            self.possibleValue.pop(ps)
+    def __updatePossibleValue(self):
+        for p in self.possibleValue:
+            if(self.possibleValue[p]==0):
+                self.possibleValue.pop(p)
+        if(len(self.possibleValue)==1):
+            v=self.possibleValue.keys()
+            self.setValue(v.pop())
     def getPossibleValue(self):
+        self.__updatePossibleValue()
         return self.possibleValue
+            
 
     def isFinish(self):
         return (self.getValue() != 0)
